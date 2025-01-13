58411ce66f8931b9cdf9d18c328c2750e3a00922
akm <akm2000@gmail.com>
2025-01-13 20:09:34 +0900

    ✅ Use github.com/stretchr/testify

--- tests/mysql/basic_test.go
+++ tests/mysql/basic_test.go
@@ -12,6 +12,8 @@ import (
 
 	sqlslog "github.com/akm/sql-slog"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestBasic(t *testing.T) {
@@ -29,9 +31,7 @@ func TestBasic(t *testing.T) {
 	buf := bytes.NewBuffer(nil)
 	logger := slog.New(slog.NewJSONHandler(buf, &slog.HandlerOptions{Level: slog.LevelDebug}))
 	db, err := sqlslog.Open(ctx, "mysql", "root@tcp(localhost:3306)/"+dbName, logger)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
 	for i := 0; i < 10; i++ {
 		if err := db.PingContext(ctx); err == nil {
@@ -41,112 +41,99 @@ func TestBasic(t *testing.T) {
 		time.Sleep(2 * time.Second)
 	}
 
-	if _, err := db.ExecContext(ctx, "CREATE TABLE IF NOT EXISTS test1 (id INT PRIMARY KEY, name VARCHAR(255))"); err != nil {
-		t.Fatal(err)
-	}
-
 	t.Run("Ping", func(t *testing.T) {
 		buf.Reset()
-		if err := db.PingContext(ctx); err != nil {
-			t.Fatal(err)
-		}
-		entries := parseJsonLines(t, buf.Bytes())
-		if len(entries) != 4 {
-			t.Fatalf("Expected 4 log entries, got %d: %+v", len(entries), entries)
-		}
+		err := db.PingContext(ctx)
+		assert.NoError(t, err)
+		actualEntries := parseJsonLines(t, buf.Bytes())
 		exptectedEntries := []map[string]interface{}{
 			{"level": "DEBUG", "msg": "ResetSession Start", "driver": "mysql", "dsn": "root@tcp(localhost:3306)/app1"},
 			{"level": "INFO", "msg": "ResetSession Complete", "driver": "mysql", "dsn": "root@tcp(localhost:3306)/app1"},
 			{"level": "DEBUG", "msg": "Ping Start", "driver": "mysql", "dsn": "root@tcp(localhost:3306)/app1"},
 			{"level": "INFO", "msg": "Ping Complete", "driver": "mysql", "dsn": "root@tcp(localhost:3306)/app1"},
 		}
+		assert.Len(t, actualEntries, len(exptectedEntries))
 		for i, expected := range exptectedEntries {
 			for k, v := range expected {
-				if entries[i][k] != v {
-					t.Fatalf("Unexpected log entry: %v", entries[i])
-				}
+				assert.Equal(t, v, actualEntries[i][k])
 			}
 		}
+	})
+
+	t.Run("create table", func(t *testing.T) {
+		result, err := db.ExecContext(ctx, "CREATE TABLE IF NOT EXISTS test1 (id INT PRIMARY KEY, name VARCHAR(255))")
+		assert.NoError(t, err)
+		rowsAffected, err := result.RowsAffected()
+		assert.NoError(t, err)
+		assert.Equal(t, int64(0), rowsAffected)
 	})
 
 	t.Run("without tx", func(t *testing.T) {
 		testData := []string{"foo", "bar", "baz"}
 		for i, name := range testData {
-			if _, err := db.ExecContext(ctx, "INSERT INTO test1 (id, name) VALUES (?, ?)", i+1, name); err != nil {
-				t.Fatal(err)
+			t.Run("insert "+name, func(t *testing.T) {
+				result, err := db.ExecContext(ctx, "INSERT INTO test1 (id, name) VALUES (?, ?)", i+1, name)
+				assert.NoError(t, err)
+				rowsAffected, err := result.RowsAffected()
+				assert.NoError(t, err)
+				assert.Equal(t, int64(1), rowsAffected)
+			})
+		}
+
+		t.Run("select", func(t *testing.T) {
+			rows, err := db.QueryContext(ctx, "SELECT id, name FROM test1 WHERE name LIKE ? ORDER BY id", "b%")
+			assert.NoError(t, err)
+
+			actualResults := []map[string]interface{}{}
+			for rows.Next() {
+				result := map[string]interface{}{}
+				var id int
+				var name string
+				if err := rows.Scan(&id, &name); err != nil {
+					t.Fatal(err)
+				}
+				result["id"] = id
+				result["name"] = name
+				actualResults = append(actualResults, result)
 			}
-		}
-
-		rows, err := db.QueryContext(ctx, "SELECT id, name FROM test1 WHERE name LIKE ? ORDER BY id", "b%")
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		results := []map[string]interface{}{}
-		for rows.Next() {
-			result := map[string]interface{}{}
-			var id int
-			var name string
-			if err := rows.Scan(&id, &name); err != nil {
-				t.Fatal(err)
+			expectedResults := []map[string]interface{}{
+				{"id": 2, "name": "bar"},
+				{"id": 3, "name": "baz"},
 			}
-			result["id"] = id
-			result["name"] = name
-			results = append(results, result)
-		}
-
-		if len(results) != 2 {
-			t.Fatalf("Expected 2 results, got %d", len(results))
-		}
-
-		if results[0]["id"] != 2 || results[0]["name"] != "bar" {
-			t.Fatalf("Unexpected result: %v", results[0])
-		}
-		if results[1]["id"] != 3 || results[1]["name"] != "baz" {
-			t.Fatalf("Unexpected result: %v", results[1])
-		}
+			assert.Equal(t, expectedResults, actualResults)
+		})
 	})
 
 	t.Run("with tx", func(t *testing.T) {
 		t.Run("rollback", func(t *testing.T) {
 			tx, err := db.BeginTx(ctx, nil)
-			if err != nil {
-				t.Fatal(err)
-			}
-			r, err := tx.ExecContext(ctx, "UPDATE test1 SET name = ? WHERE id = ?", "quux", 3)
-			if err != nil {
-				t.Fatal(err)
-			}
-			rowsAffected, err := r.RowsAffected()
-			if err != nil {
-				t.Fatal(err)
-			}
-			if rowsAffected != 1 {
-				t.Fatalf("Expected 1 row affected, got %d", rowsAffected)
-			}
-			if err := tx.Rollback(); err != nil {
-				t.Fatal(err)
-			}
+			assert.NoError(t, err)
+			t.Run("update", func(t *testing.T) {
+				r, err := tx.ExecContext(ctx, "UPDATE test1 SET name = ? WHERE id = ?", "quux", 3)
+				assert.NoError(t, err)
+				rowsAffected, err := r.RowsAffected()
+				assert.NoError(t, err)
+				assert.Equal(t, int64(1), rowsAffected)
+			})
+			t.Run("rollback", func(t *testing.T) {
+				err := tx.Rollback()
+				assert.NoError(t, err)
+			})
 		})
 		t.Run("commit", func(t *testing.T) {
 			tx, err := db.BeginTx(ctx, nil)
-			if err != nil {
-				t.Fatal(err)
-			}
-			r, err := tx.ExecContext(ctx, "UPDATE test1 SET name = ? WHERE id = ?", "qux", 3)
-			if err != nil {
-				t.Fatal(err)
-			}
-			rowsAffected, err := r.RowsAffected()
-			if err != nil {
-				t.Fatal(err)
-			}
-			if rowsAffected != 1 {
-				t.Fatalf("Expected 1 row affected, got %d", rowsAffected)
-			}
-			if err := tx.Commit(); err != nil {
-				t.Fatal(err)
-			}
+			assert.NoError(t, err)
+			t.Run("update", func(t *testing.T) {
+				r, err := tx.ExecContext(ctx, "UPDATE test1 SET name = ? WHERE id = ?", "qux", 3)
+				assert.NoError(t, err)
+				rowsAffected, err := r.RowsAffected()
+				assert.NoError(t, err)
+				assert.Equal(t, int64(1), rowsAffected)
+			})
+			t.Run("commit", func(t *testing.T) {
+				err := tx.Commit()
+				assert.NoError(t, err)
+			})
 		})
 
 	})
