835be7c44fce9eb359c44309d4c31903a44deceb
akm <akm2000@gmail.com>
2025-01-15 00:41:13 +0900

    ✅ Add test for Open function

---
+++ open_test.go
@@ -0,0 +1,25 @@
+package sqlslog
+
+import (
+	"bytes"
+	"context"
+	"log/slog"
+	"testing"
+)
+
+func TestOpen(t *testing.T) {
+	ctx := context.TODO()
+	buf := bytes.NewBuffer(nil)
+	logger := slog.New(slog.NewJSONHandler(buf, &slog.HandlerOptions{Level: slog.LevelDebug}))
+
+	db, err := Open(ctx, "invalid-driver", "", logger)
+	if err == nil {
+		t.Fatal("Expected error")
+	}
+	if err.Error() != "sql: unknown driver \"invalid-driver\" (forgotten import?)" {
+		t.Fatalf("Unexpected error: %v", err)
+	}
+	if db != nil {
+		t.Fatal("Expected nil db")
+	}
+}
