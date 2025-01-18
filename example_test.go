befcdefc49a077f32b4d9ddc0f9b5598072d1ebf
akm <akm2000@gmail.com>
2025-01-18 21:11:05 +0900

    ✅ Add examples to example_test.go

--- example_test.go
+++ example_test.go
@@ -2,6 +2,8 @@ package sqlslog_test
 
 import (
 	"context"
+	"log/slog"
+	"os"
 
 	sqlslog "github.com/akm/sql-slog"
 	// _ "github.com/mattn/go-sqlite3"
@@ -15,6 +17,52 @@ func ExampleOpen() {
 		// Handle error
 	}
 	defer db.Close()
-
 	// Use db as a regular *sql.DB
+}
+
+func ExampleOpen_withLevel() {
+	dsn := "file::memory:?cache=shared"
+	ctx := context.TODO()
+	logger := slog.New(sqlslog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.Level(sqlslog.LevelTrace),
+	}))
+	db, _ := sqlslog.Open(ctx, "sqlite3", dsn, sqlslog.Logger(logger))
+	defer db.Close()
+}
+
+func ExampleLogger() {
+	dsn := "file::memory:?cache=shared"
+	ctx := context.TODO()
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	db, _ := sqlslog.Open(ctx, "sqlite3", dsn, sqlslog.Logger(logger))
+	defer db.Close()
+}
+
+func ExampleNewJSONHandler() {
+	dsn := "file::memory:?cache=shared"
+	ctx := context.TODO()
+	logger := slog.New(sqlslog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	}))
+	db, _ := sqlslog.Open(ctx, "sqlite3", dsn, sqlslog.Logger(logger))
+	defer db.Close()
+}
+
+func ExampleNewTextHandler() {
+	dsn := "file::memory:?cache=shared"
+	ctx := context.TODO()
+	logger := slog.New(sqlslog.NewTextHandler(os.Stdout, nil))
+	db, _ := sqlslog.Open(ctx, "sqlite3", dsn, sqlslog.Logger(logger))
+	defer db.Close()
+}
+
+func ExampleSetProcNameFormatter() {
+	sqlslog.SetProcNameFormatter(func(name string, event sqlslog.ProcEvent) string {
+		return name + "/" + event.String()
+	})
+	dsn := "file::memory:?cache=shared"
+	ctx := context.TODO()
+	logger := slog.New(sqlslog.NewJSONHandler(os.Stdout, nil))
+	db, _ := sqlslog.Open(ctx, "sqlite3", dsn, sqlslog.Logger(logger))
+	defer db.Close()
 }
