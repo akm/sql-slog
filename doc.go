572dfe9159b5031759f605dd75827e37788a474b
akm <akm2000@gmail.com>
2025-01-18 21:10:35 +0900

    💡 Add comment for package

---
+++ doc.go
@@ -0,0 +1,23 @@
+/*
+sqlslog is a logger for Go SQL database driver without modify existing [*sql.DB] stdlib usage.
+sqlslog uses [*slog.Logger] to log the SQL database driver operations.
+
+# How to use
+
+	ctx := context.TODO() // or a context.Context
+	db, err := sqlslog.Open(ctx, "mysql", dsn)
+
+You can also use options to customize the logger behavior.
+
+# Options
+
+[Logger]: sets the slog.Logger to be used. If not set, the default is slog.Default().
+
+[ProcOptions]: sets the options for the logging behavior.
+
+[SetProcNameFormatter]: sets the function to format the procedure name.
+
+[*sql.DB]: https://pkg.go.dev/database/sql#DB
+[*slog.Logger]: https://pkg.go.dev/log/slog#Logger
+*/
+package sqlslog
