3195f1623ec968c15884fadbb4c608cc1ecfb2a1
akm <akm2000@gmail.com>
2025-01-18 23:07:42 +0900

    ♻️ Rename argument name of logAction and logActionContext from proc to step

--- logger.go
+++ logger.go
@@ -22,29 +22,29 @@ func (x *logger) With(kv ...interface{}) *logger {
 	return newLogger(x.Logger.With(kv...), x.options)
 }
 
-func (x *logger) logAction(proc *StepOptions, fn func() error) error {
+func (x *logger) logAction(step *StepOptions, fn func() error) error {
 	ctx := context.Background()
-	x.Log(ctx, slog.Level(proc.Start.Level), proc.Start.Msg)
+	x.Log(ctx, slog.Level(step.Start.Level), step.Start.Msg)
 	t0 := time.Now()
 	err := fn()
 	lg := x.With(slog.Int64("duration", time.Since(t0).Nanoseconds()))
 	if err != nil {
-		lg.Log(ctx, slog.Level(proc.Error.Level), proc.Error.Msg, "error", err)
+		lg.Log(ctx, slog.Level(step.Error.Level), step.Error.Msg, "error", err)
 		return err
 	}
-	lg.Log(ctx, slog.Level(proc.Complete.Level), proc.Complete.Msg)
+	lg.Log(ctx, slog.Level(step.Complete.Level), step.Complete.Msg)
 	return nil
 }
 
-func (x *logger) logActionContext(ctx context.Context, proc *StepOptions, fn func() error) error {
-	x.Log(ctx, slog.Level(proc.Start.Level), proc.Start.Msg)
+func (x *logger) logActionContext(ctx context.Context, step *StepOptions, fn func() error) error {
+	x.Log(ctx, slog.Level(step.Start.Level), step.Start.Msg)
 	t0 := time.Now()
 	err := fn()
 	lg := x.With(slog.Int64("duration", time.Since(t0).Nanoseconds()))
 	if err != nil {
-		lg.Log(ctx, slog.Level(proc.Error.Level), proc.Error.Msg, "error", err)
+		lg.Log(ctx, slog.Level(step.Error.Level), step.Error.Msg, "error", err)
 		return err
 	}
-	lg.Log(ctx, slog.Level(proc.Complete.Level), proc.Complete.Msg)
+	lg.Log(ctx, slog.Level(step.Complete.Level), step.Complete.Msg)
 	return nil
 }
