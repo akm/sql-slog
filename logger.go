c2d171af80b6446913733349b2e1dae42287b2ec
akm <akm2000@gmail.com>
2025-01-19 13:47:26 +0900

    ♻️ Use Step from StepWithoutContext

--- logger.go
+++ logger.go
@@ -23,17 +23,7 @@ func (x *logger) With(kv ...interface{}) *logger {
 }
 
 func (x *logger) StepWithoutContext(step *StepOptions, fn func() error) error {
-	ctx := context.Background()
-	x.Log(ctx, slog.Level(step.Start.Level), step.Start.Msg)
-	t0 := time.Now()
-	err := fn()
-	lg := x.With(slog.Int64("duration", time.Since(t0).Nanoseconds()))
-	if err != nil {
-		lg.Log(ctx, slog.Level(step.Error.Level), step.Error.Msg, "error", err)
-		return err
-	}
-	lg.Log(ctx, slog.Level(step.Complete.Level), step.Complete.Msg)
-	return nil
+	return x.Step(context.Background(), step, fn)
 }
 
 func (x *logger) Step(ctx context.Context, step *StepOptions, fn func() error) error {
