a28db69d130114d77b4a31e228f33db28f374221
akm <akm2000@gmail.com>
2025-01-21 22:42:03 +0900

    ✅ Add test for wrapCon

--- conn_test.go
+++ conn_test.go
@@ -1,14 +1,45 @@
 package sqlslog
 
 import (
+	"database/sql/driver"
 	"fmt"
 	"testing"
 )
 
+type mockConnForWrapConn struct {
+}
+
+// Begin implements driver.Conn.
+func (m *mockConnForWrapConn) Begin() (driver.Tx, error) {
+	panic("unimplemented")
+}
+
+// Close implements driver.Conn.
+func (m *mockConnForWrapConn) Close() error {
+	panic("unimplemented")
+}
+
+// Prepare implements driver.Conn.
+func (m *mockConnForWrapConn) Prepare(query string) (driver.Stmt, error) {
+	panic("unimplemented")
+}
+
+var _ driver.Conn = (*mockConnForWrapConn)(nil)
+
 func TestWrapConn(t *testing.T) {
-	if wrapConn(nil, nil) != nil {
-		t.Fatal("Expected nil")
-	}
+	t.Run("nil", func(t *testing.T) {
+		if wrapConn(nil, nil) != nil {
+			t.Fatal("Expected nil")
+		}
+	})
+	t.Run("implements driver.Conn but not connWithContext", func(t *testing.T) {
+		mock := &mockConnForWrapConn{}
+		logger := &logger{}
+		conn := wrapConn(mock, logger)
+		if conn == nil {
+			t.Fatal("Expected non-nil")
+		}
+	})
 }
 
 func TestConnExecContextErrorHandler(t *testing.T) {
