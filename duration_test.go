041c7bfdf68bccad89f9e93e6ab1f1f3b77829fd
akm <akm2000@gmail.com>
2025-01-22 20:44:55 +0900

    ✅ Add tests for time.Duration.String

---
+++ duration_test.go
@@ -0,0 +1,64 @@
+package sqlslog
+
+import (
+	"testing"
+	"time"
+)
+
+func TestDurationString(t *testing.T) {
+	testcases := []struct {
+		gen      func() time.Duration
+		expected string
+	}{
+		{
+			gen:      func() time.Duration { return time.Duration(0) },
+			expected: "0s",
+		},
+		{
+			gen:      func() time.Duration { return time.Duration(1) },
+			expected: "1ns",
+		},
+		{
+			gen:      func() time.Duration { return time.Duration(1e3) },
+			expected: "1µs",
+		},
+		{
+			gen:      func() time.Duration { return time.Duration(1e6) },
+			expected: "1ms",
+		},
+		{
+			gen:      func() time.Duration { return time.Duration(1e9) },
+			expected: "1s",
+		},
+		{
+			gen:      func() time.Duration { return time.Duration(1e9 + 1) },
+			expected: "1.000000001s",
+		},
+		{
+			gen:      func() time.Duration { return time.Duration(1e9 + 1e3) },
+			expected: "1.000001s",
+		},
+		{
+			gen:      func() time.Duration { return time.Duration(1e9 * 60) },
+			expected: "1m0s",
+		},
+		{
+			gen:      func() time.Duration { return time.Duration(1e9*60 + 1) },
+			expected: "1m0.000000001s",
+		},
+		{
+			gen:      func() time.Duration { return time.Duration(1e9 * 60 * 60) },
+			expected: "1h0m0s",
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.expected, func(t *testing.T) {
+			d := tc.gen()
+			actual := d.String()
+			if actual != tc.expected {
+				t.Errorf("expected: %s, but got %s", tc.expected, actual)
+			}
+		})
+	}
+}
