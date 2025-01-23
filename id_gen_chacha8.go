45691c0c622a0c75d83fc9370fe9720acbb916d4
akm <akm2000@gmail.com>
2025-01-24 07:51:33 +0900

    💡 Add comment to ChaCha8IDGenerator

--- id_gen_chacha8.go
+++ id_gen_chacha8.go
@@ -6,11 +6,13 @@ import (
 	"math/rand/v2"
 )
 
+// ChaCha8IDGenerator generates random IDs using ChaCha8 in math/rand/v2.
 type ChaCha8IDGenerator struct {
 	*rand.Rand
 	length int
 }
 
+// NewChaCha8IDGenerator creates a new ChaCha8IDGenerator with a random seed.
 func NewChaCha8IDGenerator(length int) *ChaCha8IDGenerator {
 	var s [32]byte
 	if _, err := cryptoRand.Read(s[:]); err != nil {
@@ -25,6 +27,7 @@ func NewChaCha8IDGenerator(length int) *ChaCha8IDGenerator {
 
 var generatorRunes = []byte("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_")
 
+// Generate generates a random ID.
 func (g *ChaCha8IDGenerator) Generate() string {
 	r := make([]byte, g.length)
 	runesLen := len(generatorRunes)
