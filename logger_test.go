213e3f11997d5288bb32c4a1962966a3d560f304
akm <akm2000@gmail.com>
2025-01-26 17:48:59 +0900

    ♻️ Rename expection to expected to follow the suggestion by misspell

--- logger_test.go
+++ logger_test.go
@@ -11,12 +11,12 @@ func TestLoggerDurationAttr(t *testing.T) {
 	testcases := []struct {
 		value        time.Duration
 		durationType DurationType
-		expection    func(t *testing.T, attr slog.Attr)
+		expected     func(t *testing.T, attr slog.Attr)
 	}{
 		{
 			value:        time.Duration(1),
 			durationType: DurationNanoSeconds,
-			expection: func(t *testing.T, attr slog.Attr) {
+			expected: func(t *testing.T, attr slog.Attr) {
 				if v, ok := attr.Value.Any().(int64); !ok {
 					t.Errorf("expected: %T, but got %T", int64(0), v)
 				} else if v != 1 {
@@ -27,7 +27,7 @@ func TestLoggerDurationAttr(t *testing.T) {
 		{
 			value:        time.Duration(2_000),
 			durationType: DurationMicroSeconds,
-			expection: func(t *testing.T, attr slog.Attr) {
+			expected: func(t *testing.T, attr slog.Attr) {
 				if v, ok := attr.Value.Any().(int64); !ok {
 					t.Errorf("expected: %T, but got %T", int64(0), v)
 				} else if v != 2 {
@@ -38,7 +38,7 @@ func TestLoggerDurationAttr(t *testing.T) {
 		{
 			value:        time.Duration(3_000_000),
 			durationType: DurationMilliSeconds,
-			expection: func(t *testing.T, attr slog.Attr) {
+			expected: func(t *testing.T, attr slog.Attr) {
 				if v, ok := attr.Value.Any().(int64); !ok {
 					t.Errorf("expected: %T, but got %T", int64(0), v)
 				} else if v != 3 {
@@ -49,7 +49,7 @@ func TestLoggerDurationAttr(t *testing.T) {
 		{
 			value:        time.Duration(4_000_000_000),
 			durationType: DurationGoDuration,
-			expection: func(t *testing.T, attr slog.Attr) {
+			expected: func(t *testing.T, attr slog.Attr) {
 				if attr.Value.Duration() != time.Duration(4_000_000_000) {
 					t.Errorf("expected: %d, but got %d", 4_000_000_000, attr.Value.Duration())
 				}
@@ -58,7 +58,7 @@ func TestLoggerDurationAttr(t *testing.T) {
 		{
 			value:        time.Duration(567_000_000),
 			durationType: DurationString,
-			expection: func(t *testing.T, attr slog.Attr) {
+			expected: func(t *testing.T, attr slog.Attr) {
 				if v, ok := attr.Value.Any().(string); !ok {
 					t.Errorf("expected: %T, but got %T", "", v)
 				} else if v != "567ms" {
@@ -69,7 +69,7 @@ func TestLoggerDurationAttr(t *testing.T) {
 		{
 			value:        time.Duration(890_000),
 			durationType: DurationType(-1),
-			expection: func(t *testing.T, attr slog.Attr) {
+			expected: func(t *testing.T, attr slog.Attr) {
 				if v, ok := attr.Value.Any().(int64); !ok {
 					t.Errorf("expected: %T, but got %T", int64(0), v)
 				} else if v != 890_000 {
@@ -82,7 +82,7 @@ func TestLoggerDurationAttr(t *testing.T) {
 		t.Run(tc.value.String(), func(t *testing.T) {
 			t.Parallel()
 			attr := newLogger(nil, &options{durationKey: key, durationType: tc.durationType}).durationAttr(tc.value)
-			tc.expection(t, attr)
+			tc.expected(t, attr)
 		})
 	}
 }
