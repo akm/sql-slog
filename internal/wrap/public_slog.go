10e71eef1cc4a7d5480c536c5b76fba427fa2289
akm <akm2000@gmail.com>
2025-01-29 21:44:45 +0900

    ⚰️ Remove unused definitions

--- internal/wrap/public_slog.go
+++ internal/wrap/public_slog.go
@@ -4,11 +4,6 @@ import (
 	"github.com/akm/sql-slog/public"
 )
 
-type ReplaceAttrFunc = public.ReplaceAttrFunc
-
 var (
 	NewJSONHandler = public.NewJSONHandler
-	// NewTextHandler     = public.NewTextHandler
-	// WrapHandlerOptions = public.WrapHandlerOptions
-	// MergeReplaceAttrs  = public.MergeReplaceAttrs
 )
