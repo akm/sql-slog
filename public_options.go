2166fa168981d44ff480af7ae6a38c0bed788210
akm <akm2000@gmail.com>
2025-01-29 20:04:00 +0900

    ♻️ Add Option function aliases to public_options.go

--- public_options.go
+++ public_options.go
@@ -11,6 +11,30 @@ var (
 	NewOptions             = public.NewOptions
 	Logger                 = public.Logger
 	SetStepLogMsgFormatter = public.SetStepLogMsgFormatter
+)
 
-	StmtQueryContext = public.StmtQueryContext
+var (
+	ConnBegin           = public.ConnBegin
+	ConnClose           = public.ConnClose
+	ConnPrepare         = public.ConnPrepare
+	ConnResetSession    = public.ConnResetSession
+	ConnPing            = public.ConnPing
+	ConnExecContext     = public.ConnExecContext
+	ConnQueryContext    = public.ConnQueryContext
+	ConnPrepareContext  = public.ConnPrepareContext
+	ConnBeginTx         = public.ConnBeginTx
+	ConnectorConnect    = public.ConnectorConnect
+	DriverOpen          = public.DriverOpen
+	DriverOpenConnector = public.DriverOpenConnector
+	SqlslogOpen         = public.SqlslogOpen
+	RowsClose           = public.RowsClose
+	RowsNext            = public.RowsNext
+	RowsNextResultSet   = public.RowsNextResultSet
+	StmtClose           = public.StmtClose
+	StmtExec            = public.StmtExec
+	StmtQuery           = public.StmtQuery
+	StmtExecContext     = public.StmtExecContext
+	StmtQueryContext    = public.StmtQueryContext
+	TxCommit            = public.TxCommit
+	TxRollback          = public.TxRollback
 )
