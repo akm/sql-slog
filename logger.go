783562eda32376d60e33814b5474722b7bc0bc6b
akm <akm2000@gmail.com>
2025-01-30 23:44:24 +0900

    ⚰️ Remove commented out logger.durationAttr

--- logger.go
+++ logger.go
@@ -56,24 +56,6 @@ func (x *logger) Step(ctx context.Context, step *StepOptions, fn func() (*slog.A
 	return attr, err
 }
 
-// func (x *logger) durationAttr(d time.Duration) slog.Attr {
-// 	key := x.options.durationKey
-// 	switch x.options.durationType {
-// 	case DurationNanoSeconds:
-// 		return slog.Int64(key, d.Nanoseconds())
-// 	case DurationMicroSeconds:
-// 		return slog.Int64(key, d.Microseconds())
-// 	case DurationMilliSeconds:
-// 		return slog.Int64(key, d.Milliseconds())
-// 	case DurationGoDuration:
-// 		return slog.Duration(key, d)
-// 	case DurationString:
-// 		return slog.String(key, d.String())
-// 	default:
-// 		return slog.Int64(key, d.Nanoseconds())
-// 	}
-// }
-
 func withNilAttr(f func() error) func() (*slog.Attr, error) {
 	return func() (*slog.Attr, error) {
 		return nil, f()
