e7f55d21cbe9cbe95269eb33768b094454f953ae
akm <akm2000@gmail.com>
2025-02-02 19:48:43 +0900

    💡 Move comment for Open from internal/wrap

--- open.go
+++ open.go
@@ -7,6 +7,27 @@ import (
 	"github.com/akm/sql-slog/internal/wrap"
 )
 
+/*
+Open opens a database specified by its driver name and a driver-specific data source name,
+and returns a new database handle with logging capabilities.
+
+ctx is the context for the open operation.
+driverName is the name of the database driver, same as the driverName in [sql.Open].
+dsn is the data source name, same as the dataSourceName in [sql.Open].
+opts are the options for logging behavior. See [Option] for details.
+
+The returned DB can be used the same way as *sql.DB from [sql.Open].
+
+See the following example for usage:
+
+[Logger]: sets the slog.Logger to be used. If not set, the default is slog.Default().
+
+[StepOptions]: sets the options for logging behavior.
+
+[SetStepLogMsgFormatter]: sets the function to format the step name.
+
+[sql.Open]: https://pkg.go.dev/database/sql#Open
+*/
 func Open(ctx context.Context, driverName, dsn string, opts ...Option) (*sql.DB, error) {
 	return wrap.Open(ctx, driverName, dsn, opts...)
 }
