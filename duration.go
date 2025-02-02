fd731cbde57f99b5f816ed1d0d1a54c2cc3c0cfe
akm <akm2000@gmail.com>
2025-02-02 19:48:05 +0900

    💡 Copy comments for Duration constants from internal/opts

--- duration.go
+++ duration.go
@@ -5,11 +5,11 @@ import "github.com/akm/sql-slog/internal/opts"
 type DurationType = opts.DurationType
 
 const (
-	DurationNanoSeconds  = opts.DurationNanoSeconds
-	DurationMicroSeconds = opts.DurationMicroSeconds
-	DurationMilliSeconds = opts.DurationMilliSeconds
-	DurationGoDuration   = opts.DurationGoDuration
-	DurationString       = opts.DurationString
+	DurationNanoSeconds  = opts.DurationNanoSeconds  // Duration in nanoseconds. Durations in log are expressed by slog.Int64
+	DurationMicroSeconds = opts.DurationMicroSeconds // Duration in microseconds. Durations in log are expressed by slog.Int64
+	DurationMilliSeconds = opts.DurationMilliSeconds // Duration in milliseconds. Durations in log are expressed by slog.Int64
+	DurationGoDuration   = opts.DurationGoDuration   // Values in log are expressed with slog.Duration
+	DurationString       = opts.DurationString       // Values in log are expressed with slog.String and time.Duration.String
 )
 
 // Duration is an option to specify duration value in log.
