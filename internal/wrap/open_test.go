be42da1220f429e237e0bb3f32be56badb039d4e
akm <akm2000@gmail.com>
2025-02-02 18:14:19 +0900

    ✅ Add test for open error case with DriverContext in order to improve coverages

--- internal/wrap/open_test.go
+++ internal/wrap/open_test.go
@@ -3,6 +3,8 @@ package wrap
 import (
 	"bytes"
 	"context"
+	"database/sql/driver"
+	"errors"
 	"log/slog"
 	"testing"
 )
@@ -23,4 +25,42 @@ func TestOpen(t *testing.T) {
 	if db != nil {
 		t.Fatal("Expected nil db")
 	}
+}
+
+type errorDriverContext struct {
+	error error
+}
+
+var (
+	_ driver.Driver        = (*errorDriverContext)(nil)
+	_ driver.DriverContext = (*errorDriverContext)(nil)
+)
+
+func newErrorDriverContext(err error) *errorDriverContext {
+	return &errorDriverContext{error: err}
+}
+
+// OpenConnector implements driver.DriverContext.
+func (e *errorDriverContext) OpenConnector(string) (driver.Connector, error) {
+	return nil, e.error
+}
+
+// Open implements driver.Driver.
+func (e *errorDriverContext) Open(string) (driver.Conn, error) {
+	return nil, e.error
+}
+
+func TestOpenWithDriver(t *testing.T) {
+	t.Parallel()
+	t.Run("unknown error", func(t *testing.T) {
+		t.Parallel()
+		t.Run("DriverContext", func(t *testing.T) {
+			drv := newErrorDriverContext(errors.New("unknown error"))
+			stepLogger := NewStepLogger(slog.New(slog.NewJSONHandler(nil, nil)), nil)
+			_, err := openWithDriver(drv, "invalid-dsn", stepLogger, nil)
+			if err == nil {
+				t.Fatal("Expected error")
+			}
+		})
+	})
 }
