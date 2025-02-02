ae6ef4f8173ddb7b21ac1b26e75c249d70bb3eac
akm <akm2000@gmail.com>
2025-02-02 11:53:08 +0900

    ♻️ Move the functions Duration and DurationKey and add functions with the same name and call each one

--- duration.go
+++ duration.go
@@ -16,16 +16,8 @@ var DurationAttrFunc = opts.DurationAttrFunc
 
 // Duration is an option to specify duration value in log.
 // The default is DurationNanoSeconds.
-func Duration(v DurationType) Option {
-	return func(o *Options) {
-		o.DurationType = v
-	}
-}
+func Duration(v DurationType) Option { return opts.Duration(v) }
 
 // DurationKey is an option to specify the key for duration value in log.
 // The default is specified by DurationKeyDefault.
-func DurationKey(key string) Option {
-	return func(o *Options) {
-		o.DurationKey = key
-	}
-}
+func DurationKey(key string) Option { return opts.DurationKey(key) }
--- opts/duration.go
+++ opts/duration.go
@@ -31,3 +31,19 @@ func DurationAttrFunc(key string, t DurationType) func(d time.Duration) slog.Att
 		return func(d time.Duration) slog.Attr { return slog.Int64(key, d.Nanoseconds()) }
 	}
 }
+
+// Duration is an option to specify duration value in log.
+// The default is DurationNanoSeconds.
+func Duration(v DurationType) Option {
+	return func(o *Options) {
+		o.DurationType = v
+	}
+}
+
+// DurationKey is an option to specify the key for duration value in log.
+// The default is specified by DurationKeyDefault.
+func DurationKey(key string) Option {
+	return func(o *Options) {
+		o.DurationKey = key
+	}
+}
