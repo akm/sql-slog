249d47b066d131e79778acc977da808a96b52549
akm <akm2000@gmail.com>
2025-02-02 19:48:16 +0900

    💡 Copy comments for Level constants from internal/opts

--- level.go
+++ level.go
@@ -4,13 +4,14 @@ import (
 	"github.com/akm/sql-slog/internal/opts"
 )
 
+// Level is the log level for sqlslog.
 type Level = opts.Level
 
 const (
-	LevelVerbose Level = opts.LevelVerbose
-	LevelTrace   Level = opts.LevelTrace
-	LevelDebug   Level = opts.LevelDebug
-	LevelInfo    Level = opts.LevelInfo
-	LevelWarn    Level = opts.LevelWarn
-	LevelError   Level = opts.LevelError
+	LevelVerbose Level = opts.LevelVerbose // Lower than slog.LevelTrace.
+	LevelTrace   Level = opts.LevelTrace   // Lower than slog.LevelDebug.
+	LevelDebug   Level = opts.LevelDebug   // Same as slog.LevelDebug.
+	LevelInfo    Level = opts.LevelInfo    // Same as slog.LevelInfo.
+	LevelWarn    Level = opts.LevelWarn    // Same as slog.LevelWarn.
+	LevelError   Level = opts.LevelError   // Same as slog.LevelError.
 )
