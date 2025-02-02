b6fb88c069451e015d4e59234e63898c7c2b6c0b
akm <akm2000@gmail.com>
2025-02-02 20:29:24 +0900

    ♻️ Move functions defined in id_gen.go to internal/opts and add  functions to call each one in id_gen.go

--- id_gen.go
+++ id_gen.go
@@ -7,16 +7,16 @@ type IDGen = opts.IDGen
 
 // IDGenerator returns an Option that sets the ID generator.
 // The default is IDGeneratorDefault.
-func IDGenerator(idGen IDGen) Option { return func(o *Options) { o.IDGen = idGen } }
+func IDGenerator(idGen IDGen) Option { return opts.IDGenerator(idGen) }
 
 // ConnIDKey sets the key for the connection ID.
 // The default is ConnIDKeyDefault.
-func ConnIDKey(key string) Option { return func(o *Options) { o.ConnIDKey = key } }
+func ConnIDKey(key string) Option { return opts.ConnIDKey(key) }
 
 // TxIDKey sets the key for the transaction ID.
 // The default is TxIDKeyDefault.
-func TxIDKey(key string) Option { return func(o *Options) { o.TxIDKey = key } }
+func TxIDKey(key string) Option { return opts.TxIDKey(key) }
 
 // StmtIDKey sets the key for the statement ID.
 // The default is StmtIDKeyDefault.
-func StmtIDKey(key string) Option { return func(o *Options) { o.StmtIDKey = key } }
+func StmtIDKey(key string) Option { return opts.StmtIDKey(key) }
--- internal/opts/id_gen.go
+++ internal/opts/id_gen.go
@@ -7,3 +7,19 @@ const defaultIDLength = 16
 
 // IDGeneratorDefault is the default ID generator.
 var IDGeneratorDefault = NewChaCha8IDGenerator(defaultIDLength).Generate
+
+// IDGenerator returns an Option that sets the ID generator.
+// The default is IDGeneratorDefault.
+func IDGenerator(idGen IDGen) Option { return func(o *Options) { o.IDGen = idGen } }
+
+// ConnIDKey sets the key for the connection ID.
+// The default is ConnIDKeyDefault.
+func ConnIDKey(key string) Option { return func(o *Options) { o.ConnIDKey = key } }
+
+// TxIDKey sets the key for the transaction ID.
+// The default is TxIDKeyDefault.
+func TxIDKey(key string) Option { return func(o *Options) { o.TxIDKey = key } }
+
+// StmtIDKey sets the key for the statement ID.
+// The default is StmtIDKeyDefault.
+func StmtIDKey(key string) Option { return func(o *Options) { o.StmtIDKey = key } }
