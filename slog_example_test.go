d8bb4de6d3c2ccb9cf2ddabbb57a96302b21e0f5
akm <akm2000@gmail.com>
2025-02-18 23:07:17 +0900

    ✅ Define Output in ExampleNewJSONHandler and ExampleNewTextHandler
    by using MockDriver and RemoveTimeAndDuration

--- slog_example_test.go
+++ slog_example_test.go
@@ -2,28 +2,50 @@ package sqlslog_test
 
 import (
 	"context"
+	"log/slog"
 
 	sqlslog "github.com/akm/sql-slog"
 	// _ "github.com/mattn/go-sqlite3"
 )
 
+func removeTimeAndDuration(groups []string, a slog.Attr) slog.Attr {
+	if len(groups) == 0 {
+		switch a.Key {
+		case slog.TimeKey:
+			return slog.Attr{}
+		case "duration":
+			return slog.Attr{}
+		}
+	}
+	return a
+}
+
 func ExampleNewJSONHandler() {
-	dsn := "file::memory:?cache=shared"
+	dsn := "dummy-dsn"
 	ctx := context.TODO()
-	db, logger, _ := sqlslog.Open(ctx, "sqlite3", dsn,
+	db, logger, _ := sqlslog.Open(ctx, "mock", dsn,
 		sqlslog.HandlerFunc(sqlslog.NewJSONHandler),
-		sqlslog.LogLevel(sqlslog.LevelDebug),
+		sqlslog.LogReplaceAttr(removeTimeAndDuration),
 	)
 	defer db.Close()
 	logger.InfoContext(ctx, "Hello, World!")
+
+	// Output:
+	// {"level":"INFO","msg":"Open","driver":"mock","dsn":"dummy-dsn"}
+	// {"level":"INFO","msg":"Hello, World!"}
 }
 
 func ExampleNewTextHandler() {
-	dsn := "file::memory:?cache=shared"
+	dsn := "dummy-dsn"
 	ctx := context.TODO()
-	db, logger, _ := sqlslog.Open(ctx, "sqlite3", dsn,
+	db, logger, _ := sqlslog.Open(ctx, "mock", dsn,
 		sqlslog.HandlerFunc(sqlslog.NewTextHandler),
+		sqlslog.LogReplaceAttr(removeTimeAndDuration),
 	)
 	defer db.Close()
 	logger.InfoContext(ctx, "Hello, World!")
+
+	// Output:
+	// level=INFO msg=Open driver=mock dsn=dummy-dsn
+	// level=INFO msg="Hello, World!"
 }
