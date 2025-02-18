ef2ddeac05dccd4548658e85bb1ac3d76f3712a9
akm <akm2000@gmail.com>
2025-02-18 23:22:03 +0900

    ✅ Add examples for sqlslog.Handler option

--- slog_example_test.go
+++ slog_example_test.go
@@ -3,6 +3,7 @@ package sqlslog_test
 import (
 	"context"
 	"log/slog"
+	"os"
 
 	sqlslog "github.com/akm/sql-slog"
 	// _ "github.com/mattn/go-sqlite3"
@@ -48,4 +49,45 @@ func ExampleNewTextHandler() {
 	// Output:
 	// level=INFO msg=Open driver=mock dsn=dummy-dsn
 	// level=INFO msg="Hello, World!"
+}
+
+func ExampleHandler() {
+	// Normal slog.Handler with sqlslog.ReplaceLevelAttr knows how to show LevelTrace and LevelVerbose.
+	handler := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		ReplaceAttr: sqlslog.MergeReplaceAttrs(
+			sqlslog.ReplaceLevelAttr,
+			removeTimeAndDuration, // for testing
+		),
+		Level: sqlslog.LevelVerbose,
+	})
+
+	dsn := "dummy-dsn"
+	ctx := context.TODO()
+
+	logger := sqlslog.New("mock", dsn, sqlslog.Handler(handler)).Logger()
+	logger.Log(ctx, slog.Level(sqlslog.LevelTrace), "Foo")
+	logger.Log(ctx, slog.Level(sqlslog.LevelVerbose), "Bar")
+
+	// Output:
+	// level=TRACE msg=Foo
+	// level=VERBOSE msg=Bar
+}
+
+func ExampleHandler_withoutReplaceLevelAttr() {
+	// Normal slog.Handler without sqlslog.ReplaceLevelAttr does not know how to show LevelTrace and LevelVerbose.
+	handler := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		ReplaceAttr: removeTimeAndDuration,
+		Level:       sqlslog.LevelVerbose,
+	})
+
+	dsn := "dummy-dsn"
+	ctx := context.TODO()
+	logger := sqlslog.New("mock", dsn, sqlslog.Handler(handler)).Logger()
+
+	logger.Log(ctx, slog.Level(sqlslog.LevelTrace), "Foo")
+	logger.Log(ctx, slog.Level(sqlslog.LevelVerbose), "Bar")
+
+	// Output:
+	// level=DEBUG-4 msg=Foo
+	// level=DEBUG-8 msg=Bar
 }
